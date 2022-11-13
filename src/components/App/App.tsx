63e5e10d25b800e7ff7b95fa4a47d63f70a04327
MobbySchiller <kb.burzynski@gmail.com>
2022-11-13 15:20:31 +0100

    First commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../Header/Header'
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <div>Test</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/data/images.ts
@@ -0,0 +1,74 @@
+import mercuryImage from '../assets/images/planet-mercury.svg';
+import mercuryInternalImage from '../assets/images/planet-mercury-internal.svg';
+import mercuryGeoImage from '../assets/images/planet-mercury-geology.png';
+
+import venusImage from '../assets/images/planet-venus.svg';
+import venusInternalImage from '../assets/images/planet-venus-internal.svg';
+import venusGeoImage from '../assets/images/planet-venus-geology.png';
+
+import earthImage from '../assets/images/planet-earth.svg';
+import earthInternalImage from '../assets/images/planet-earth-internal.svg';
+import earthGeoImage from '../assets/images/planet-earth-geology.png';
+
+import marsImage from '../assets/images/planet-mars.svg';
+import marsInternalImage from '../assets/images/planet-mars-internal.svg';
+import marsGeoImage from '../assets/images/planet-mars-geology.png';
+
+import jupiterImage from '../assets/images/planet-jupiter.svg';
+import jupiterInternalImage from '../assets/images/planet-jupiter-internal.svg';
+import jupiterGeoImage from '../assets/images/planet-jupiter-geology.png';
+
+import saturnImage from '../assets/images/planet-saturn.svg';
+import saturnInternalImage from '../assets/images/planet-saturn-internal.svg';
+import saturnGeoImage from '../assets/images/planet-saturn-geology.png';
+
+import uranusImage from '../assets/images/planet-uranus.svg';
+import uranusInternalImage from '../assets/images/planet-uranus-internal.svg';
+import uranusGeoImage from '../assets/images/planet-uranus-geology.png';
+
+import neptuneImage from '../assets/images/planet-neptune.svg';
+import neptuneInternalImage from '../assets/images/planet-neptune-internal.svg';
+import neptuneGeoImage from '../assets/images/planet-neptune-geology.png';
+
+export const planetImages = {
+    mercury: {
+        full: mercuryImage,
+        internal: mercuryInternalImage,
+        geo: mercuryGeoImage,
+    },
+    venus: {
+        full: venusImage,
+        internal: venusInternalImage,
+        geo: venusGeoImage,
+    },
+    earth: {
+        full: earthImage,
+        internal: earthInternalImage,
+        geo: earthGeoImage,
+    },
+    mars: {
+        full: marsImage,
+        internal: marsInternalImage,
+        geo: marsGeoImage,
+    },
+    jupiter: {
+        full: jupiterImage,
+        internal: jupiterInternalImage,
+        geo: jupiterGeoImage,
+    },
+    saturn: {
+        full: saturnImage,
+        internal: saturnInternalImage,
+        geo: saturnGeoImage,
+    },
+    uranus: {
+        full: uranusImage,
+        internal: uranusInternalImage,
+        geo: uranusGeoImage,
+    },
+    neptune: {
+        full: neptuneImage,
+        internal: neptuneInternalImage,
+        geo: neptuneGeoImage,
+    },
+}
