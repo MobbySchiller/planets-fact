14058178f955c1d2c77e0d917a1208704df9dcb5
MobbySchiller <kb.burzynski@gmail.com>
2022-11-14 16:33:34 +0100

    Add container

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from '../Header/Header'
+import './App.scss'
 
 const App: React.FC = () => {
   return (
-    <>
+    <div className='container'>
       <Header />
       <div>Test</div>
-    </>
+    </div>
   )
 }
 
